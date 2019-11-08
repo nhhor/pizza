3a23306727712dbff8cc504897566470050668d3
Noah Horwitz <00noah@gmail.com>
2019-11-08 14:01:53 -0800

    add price prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -3,34 +3,58 @@
 function Order() {
   this.pizzas = [],
   this.currentId = 0
-}
+};
 
 Order.prototype.addPizza = function(pizza) {
   pizza.id = this.assignId();
   this.pizzas.push(pizza);
-}
+  pizza.price = this.assignPrice(pizza);
+};
 
 Order.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
-}
+};
+
+Order.prototype.assignPrice = function(pizza) {
+  if (pizza.pizzaSize === "smallSize") {
+    toppingCount = 0
+    pizza.toppingsList.forEach(function(topping){
+      toppingCount += 1;
+      return toppingCount
+    });
+    return toppingCount + 8;
+  }
+  else if (pizza.pizzaSize === "largeSize") {
+    toppingCount = 0
+    pizza.toppingsList.forEach(function(topping){
+      toppingCount += 2;
+      return toppingCount
+    });
+    return toppingCount + 14;  }
+  else {
+    return "Problem!";
+  }
+};
 
 function Pizza (pizzaSize, toppingsList, firstName, lastName, phoneNumber) {
   this.pizzaSize = pizzaSize,
   this.toppingsList = toppingsList,
   this.firstName = firstName,
   this.lastName = lastName,
-  this.phoneNumber = phoneNumber,
-  this.cost = 0
-}
+  this.phoneNumber = phoneNumber
+};
 
-
-// Order.prototype.toppingsListPrint = function(){
-//
-//
-//   console.log(order);
-//   // this.toppingsList.forEach(function(topping){
-//   // });
+// Order.prototype.toppingsListPrint = function(order){
+//   var pizzaDisplay2 = "";
+//   var htmlForToppingDisplay = "";
+//   order.pizzas.forEach(function(pizza){
+//     pizza.toppingsList.forEach(function(topping){
+//       console.log(topping);
+//       htmlForToppingDisplay += '<li class="list-group-item">'+topping+'</li>';
+//       })
+//   });
+//   pizzaDisplay2.html(htmlForPizzaDisplay);
 // };
 
 
@@ -45,10 +69,7 @@ function displayPizzas(order) {
   var htmlForPizzaDisplay = "";
   var htmlForToppingDisplay = "";
   order.pizzas.forEach(function(pizza) {
-    htmlForPizzaDisplay += '<div class="col-md-4 mb-4"><div class="card"><div class="card-header bg-primary">Pizza # '+ pizza.id +'</div><div class="card-body bg-info"><ul class="list-group list-group-flush "><li class="list-group-item">Toppings:</li><li class="list-group-item">'+pizza.toppingsList.forEach(function(topping){
-      console.log(topping);
-      return htmlForToppingDisplay += topping;
-      })+'</li></ul></div><div class="card-footer bg-secondary">Price: </div></div></div>';
+    htmlForPizzaDisplay += '<div class="col-md-4 mb-4"><div class="card"><div class="card-header bg-primary">Pizza # '+ pizza.id +'</div><div class="card-body bg-info"><ul class="list-group list-group-flush "><li class="list-group-item">Toppings:</li><li class="list-group-item">'+pizza.toppingsList+'</li></ul></div><div class="card-footer bg-secondary">Price: </div></div></div>';
   });
   pizzaDisplay.html(htmlForPizzaDisplay);
 };
